5b1b56d58d90958dae15c1ed7dd9a288818a466d
Maximiliano Contreras <57560155+Senior-S@users.noreply.github.com>
2020-09-01 21:06:33 -0300

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+
+namespace HiddenIdentity
+{
+    public class PluginConfiguration : IRocketPluginConfiguration
+    {
+        public string hidden_masks;
+        public string override_permission;
+        public void LoadDefaults()
+        {
+            hidden_masks = "1048,1270,434,441";
+            override_permission = "ss.overridepermission";
+        }
+    }
+}
---
+++ HiddenIdentity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Core.Utils;
+using SDG.Unturned;
+
+namespace HiddenIdentity
+{
+    public class HiddenIdentity : RocketPlugin<PluginConfiguration>
+    {
+        protected override void Load()
+        {
+            Logger.Log("[HiddenIdentity] Plugin loaded correctly!");
+            Logger.Log("If you have any error you can contact the owner in discord: Senior S#9583");
+            Provider.onEnemyConnected += OnEnemyConnected;
+            Provider.onEnemyDisconnected += OnEnemyDisconnected;
+        }
+
+        private void OnEnemyConnected(SteamPlayer player)
+        {
+            player.player.clothing.onMaskUpdated += (newMask, newMaskQuality, newMaskState) => OnMaskUpdated(player.player, newMask, newMaskQuality, newMaskState);
+        }
+
+        private void OnEnemyDisconnected(SteamPlayer player)
+        {
+            player.player.clothing.onMaskUpdated -= (newMask, newMaskQuality, newMaskState) => OnMaskUpdated(player.player, newMask, newMaskQuality, newMaskState);
+        }
+
+        private void OnMaskUpdated(Player player, ushort newMask, byte newMaskQuality, byte[] newMaskState)
+        {
+            var ids = Configuration.Instance.hidden_masks.Split(',');
+            if (ids.Contains(newMask.ToString()))
+            {
+                List<Player> client = new List<Player>();
+                PlayerTool.getPlayersInRadius(player.movement.move, 15f, client);
+                for (int i = 0; i < client.Count; i++)
+                {
+                    if (client[i] == player) return;
+                    Player victimPlayer = RaycastHelper.GetPlayer(client[i], 15);
+                    if (victimPlayer == player)
+                    {
+                        client[i].setPluginWidgetFlag(EPluginWidgetFlags.ShowInteractWithEnemy, false);
+                        TaskDispatcher.QueueOnMainThread(CheckPlayer(client[i], player));
+                    }
+                }
+            }
+        }
+
+        public System.Action CheckPlayer(Player user, Player mask)
+        {
+            Task.Delay(TimeSpan.FromSeconds(3));
+            if (user.look.player == mask)
+            {
+                TaskDispatcher.QueueOnMainThread(CheckPlayer(user, mask));
+                return null;
+            }
+            else
+            {
+                user.setPluginWidgetFlag(EPluginWidgetFlags.ShowInteractWithEnemy, true);
+                return null;
+            }
+        }
+    }
+}
